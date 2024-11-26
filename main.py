be35ec78758d10a008ce38898d457c2e2c55a897
Pavel8 <pvlkadlec@gmail.com>
2024-11-26 19:25:56 +0100

    pokus

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hi!")
+from abc import ABC, abstractmethod
+
+class View(ABC):
+    def __init__(self, name: str, position: tuple):
+        self.name = name
+        self.position = position
+
+    @abstractmethod
+    def move(self, new_position: tuple):
+
+class Zobrazitelny:
+
+    @abstractmethod
+    def show(self):
+        print("Informace")
+
+class Button(View, Zobrazitelny):
+
